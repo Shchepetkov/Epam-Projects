926341be5f3bfb47ea2a22acd742aec8b1fbbaba
Shchepetkov <maksim.shchepetkov1995@gmail.com>
2021-03-23 23:57:29 +0400

    Add Task 3.1.1

--- Task 2/Task 2.2/Canvas/GameField.cs
+++ Task 2/Task 2.2/Canvas/GameField.cs
@@ -25,7 +25,6 @@ namespace game.Canvas
          {
             snake.Move();
 
-
             if (Console.KeyAvailable)
             {
                ConsoleKeyInfo key = Console.ReadKey();
---
+++ Task 3/Task 3.1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epam
+{
+   class Program
+   {
+      static void Main()
+      {
+         Console.Write("Введите N: ");
+         int Round = 1;
+         int N = Convert.ToInt32(Console.ReadLine());
+
+         var List = new LinkedList<int>(Enumerable.Range(1, N));
+         Console.WriteLine("Сгенерирован круг людей. Начинаем вычеркивать каждого второго.");
+         // Console.WriteLine(string.Join(", ", list)); // - вывод элементов листа от 1 до n
+         var currentItem = List.First;
+         while (List.Count != 1)
+         {
+            List.Remove(currentItem.Next ?? List.First);
+            currentItem = currentItem.Next ?? List.First;
+            Console.WriteLine($"Раунд {Round}. Вычеркнут человек. Людей осталось: {List.Count}.");
+            Round++;
+         }
+         Console.WriteLine(List.First.Value);
+         // currentItem.Value - элементы которые удалили
+      }
+   }
+}
